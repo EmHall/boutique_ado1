ce374245ed06d245579dc27633156e6b7d6c5e90
EmHall <em.cm.hall@gmail.com>
2020-10-15 16:07:09 +0000

    customised admin

--- boutique_ado1/settings.py
+++ boutique_ado1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
